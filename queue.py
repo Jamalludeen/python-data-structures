2e549568de2ad7d1b00ae342a63274f41257cbd7
Jamalludeen <jamalghazniwal@gmail.com>
2025-12-27 20:58:45 +0430

    Implement traverse, is_empty & get_items method of queue

--- queue.py
+++ queue.py
@@ -27,3 +27,21 @@ class Queue:
             return self.queue[0]
         else:
             return "Queue is empty!"
+    
+    def traverse(self):
+        if len(self.queue):
+            for value in self.queue:
+                print(value)
+        else:
+            print("Queue is empty!")
+    
+            
+    def is_empty(self):
+        if len(self.queue):
+            return False
+        else:
+            return True
+     
+            
+    def get_items(self):
+        return f'Queue -> {self.queue}'
