e4014c5f07fea3550fd51419b3a23ffe221eba08
Jamalludeen <jamalghazniwal@gmail.com>
2025-12-27 20:53:11 +0430

    Implement Queue data structure

---
+++ queue.py
@@ -0,0 +1,20 @@
+class Queue:
+    def __init__(self, size):
+        self.get_size = size
+        self.queue = [] 
+        self.top = 0
+    
+    def enqueue(self, value):
+        # self.queue.appendleft(value)
+        if len(self.queue) < self.get_size:
+            self.queue.append(value)
+        else:
+            print("Queue is full!")
+
+    def dequeue(self):
+        if len(self.queue):
+            return self.queue.pop()
+        else:
+            return "Queue is empty!"
+        
+        
