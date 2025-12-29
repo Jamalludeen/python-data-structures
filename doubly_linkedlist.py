993e8247a3fa51c11de6a5496b9100e11eab1c55
Jamalludeen <jamalghazniwal@gmail.com>
2025-12-29 09:44:05 +0430

    Add insert_end method to Doubly Linked List

--- doubly_linkedlist.py
+++ doubly_linkedlist.py
@@ -19,4 +19,15 @@ class DoublyLinkedList:
         else:
             self.head = self.head = new_node
         self.length += 1
-        
+
+
+    def insert_end(self, data):
+        new_node = Node(data=data)
+        if self.tail:
+            new_node.prev = self.tail
+            self.tail.next = new_node
+            self.tail = new_node
+        else:
+            self.head = self.tail = new_node
+        self.length += 1
+
