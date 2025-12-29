376abcaecef7e6772bd1b9cd9948b1d287d86442
Jamalludeen <jamalghazniwal@gmail.com>
2025-12-29 20:30:59 +0430

    Implement forward traversal method to Doubly Linked List and fixed insertion bug

--- doubly_linkedlist.py
+++ doubly_linkedlist.py
@@ -17,13 +17,13 @@ class DoublyLinkedList:
             self.head.prev = new_node
             self.head = new_node
         else:
-            self.head = self.head = new_node
+            self.head = self.tail = new_node
         self.length += 1
 
     def insert_at_position(self, index, data):
-        if index < 0:
+        if index <= 0:
             self.insert_start(data=data)
-        elif index > self.length-1:
+        elif index >= self.length-1:
             self.insert_end(data=data)
         
         else:
@@ -47,3 +47,21 @@ class DoublyLinkedList:
             self.head = self.tail = new_node
         self.length += 1
 
+    def traverse_forward(self):
+        if not self.head:
+            return 'Linked list is empty!'
+
+        result = ''
+        temp = self.head
+        while temp:
+            result += str(temp.data)+ ' <-> '
+            temp = temp.next
+        return result.rstrip(' <-> ')
+
+
+if __name__ == "__main__":
+    dll = DoublyLinkedList()
+    dll.insert_start(10)
+    dll.insert_end(90)
+    dll.insert_at_position(0, 77)
+    print(dll.traverse_forward())
